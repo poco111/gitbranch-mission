de8632befc110e8db512a3553224e19aee855aea
Poco <101160636+poco111@users.noreply.github.com>
2022-11-23 01:13:52 +0900

    Q4. sum branch

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
+let sum = 0;
+
 for (let i = 1; i <= 10; i++) {
-  if (i % 2 === 0) console.log(i);
+  if (i % 2 === 0) {
+    console.log(i);
+    sum += i;
+  }
 }
+console.log(`총 합 : ${sum}`);
 console.log(`끝났다`);
